deb1fcd2fe450bc562d388e219a6a5a9e9ea7527
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-09-15 15:54:16 -0300

    3rd day, almost finished

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,16 +1,87 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client"
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
-app.get("/", (req, res) =>
-  res.json([
-    {
-      id: 1,
-      name: "1",
+app.route("/").get(function (req, res) {
+  res.status(200).json([])
+})
+
+app
+  .route("/ads")
+  .get(function (req, res) {
+    res.status(200).json([])
+  })
+  .post(function (req, res) {
+    res.status(201).json([])
+  })
+
+app
+  .route("/games")
+  .get(async (req, res) => {
+    const games = await prisma.game.findMany({
+      include: {
+        _count: {
+          select: {
+            ads: true,
+          },
+        },
+      },
+    })
+
+    res.status(200).json(games)
+  })
+  .post(function (req, res) {
+    res.status(201).json([])
+  })
+
+app.route("/games/:id/ads").get(async function (req, res) {
+  const gameId = req.params.id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      HourEnd: true,
     },
-  ])
-)
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  })
+
+  res.status(200).json(
+    ads.map((ad) => ({
+      ...ad,
+      weekDays: ad.weekDays.split(","),
+    }))
+  )
+})
+
+app
+  .route("/ads/:id/discord")
+  .get(async function (req, res) {
+    const adId = req.params.id
+
+    const ad = await prisma.ad.findUniqueOrThrow({
+      where: { id: adId },
+      select: {
+        discord: true,
+      },
+    })
+
+    res.status(200).json({
+      discord: ad.discord,
+    })
+  })
 
 app.listen("3333")
